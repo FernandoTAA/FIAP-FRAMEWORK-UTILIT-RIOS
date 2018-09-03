e8cefd20f1d3457fb6a80961cb0d7e7ec8228fb4
Fernando Teixeira Alves de Araujo <fernandoftaa@yahoo.com.br>
2018-09-03 09:42:41 -0300

    Inclusão de Log4j e Testes de Stress

--- roupas/src/main/java/br/com/fiap/roupas/service/InvoicePdfService.java
+++ roupas/src/main/java/br/com/fiap/roupas/service/InvoicePdfService.java
@@ -3,16 +3,19 @@ package br.com.fiap.roupas.service;
 import br.com.fiap.roupas.builder.InvoiceBuilder;
 import br.com.fiap.roupas.builder.TextPDFBuilder;
 import br.com.fiap.roupas.entity.Invoice;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.input.ReaderInputStream;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.*;
-import java.time.LocalDateTime;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+@Log4j2
 @Service
 public class InvoicePdfService {
 
@@ -21,6 +24,7 @@ public class InvoicePdfService {
 
     @Transactional
     public void generatePdfFile(Long invoiceCounter) {
+        log.info(String.format("BEGIN: generatePdfFile(%d)", invoiceCounter));
         Invoice invoice = invoiceService.getInvoice(invoiceCounter);
 
         InvoiceBuilder invoiceBuilder = new InvoiceBuilder();
@@ -40,9 +44,12 @@ public class InvoicePdfService {
         textPDFBuilder.setText(invoiceText);
 
         textPDFBuilder.build();
+        log.info(String.format("END: generatePdfFile(%d)", invoiceCounter));
     }
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public byte[] getPdfBinary(Long invoiceCounter) {
+        log.info(String.format("BEGIN: getPdfBinary(%d)", invoiceCounter));
         File file = getPdfFile(invoiceCounter);
 
         if (!file.exists() && !file.isFile()) {
@@ -50,14 +57,17 @@ public class InvoicePdfService {
         }
 
         try (InputStream inputStream = new FileInputStream(file)) {
-            return IOUtils.toByteArray(inputStream);
+            byte[] binary = IOUtils.toByteArray(inputStream);
+            log.info(String.format("END: getPdfBinary(%d)", invoiceCounter));
+            return binary;
         } catch (IOException e) {
+            log.error(String.format("ERROR: getPdfBinary(%d)", invoiceCounter), e);
             throw new RuntimeException(e);
         }
     }
 
     private File getPdfFile(Long invoiceCounter) {
-        String fileName = String.format("invoice-%d.pdf", invoiceCounter);
+        String fileName = String.format("invoice-%d", invoiceCounter);
         return new File(String.format("./invoices-files/%s.pdf", fileName));
     }
 }
--- roupas/src/main/java/br/com/fiap/roupas/service/InvoiceService.java
+++ roupas/src/main/java/br/com/fiap/roupas/service/InvoiceService.java
@@ -3,6 +3,8 @@ package br.com.fiap.roupas.service;
 import br.com.fiap.roupas.entity.Invoice;
 import br.com.fiap.roupas.producer.InvoicePdfGeneratorProducer;
 import br.com.fiap.roupas.repository.InvoiceRepository;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Log4j2
 @Service
 public class InvoiceService {
 
@@ -27,39 +30,55 @@ public class InvoiceService {
 
     @Transactional
     public Invoice saveInvoice(Invoice invoice) {
-        return invoiceRepository.save(invoice);
+        log.info(String.format("BEGIN: saveInvoice(%d)", invoice.getInvoiceCounter()));
+        Invoice savedInvoice = invoiceRepository.save(invoice);
+        log.info(String.format("END: saveInvoice(%d)", invoice.getInvoiceCounter()));
+        return savedInvoice;
     }
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public Invoice getInvoice(Long invoiceCounter) {
-        return invoiceRepository.findFirstByInvoiceCounter(invoiceCounter);
+        log.info(String.format("BEGIN: getInvoice(%d)", invoiceCounter));
+        Invoice invoice = invoiceRepository.findFirstByInvoiceCounter(invoiceCounter);
+        log.info(String.format("END: getInvoice(%d)", invoiceCounter));
+        return invoice;
     }
 
     @Transactional
     public Boolean generatePdfById(Long invoiceCounter) {
         try {
+            log.info(String.format("BEGIN: generatePdfById(%d)", invoiceCounter));
             invoicePdfGeneratorProducer.publish(invoiceCounter);
+            log.info(String.format("END: generatePdfById(%d)", invoiceCounter));
             return true;
         } catch (Exception e) {
+            log.error(String.format("ERROR: generatePdfById(%d)", invoiceCounter), e);
             return false;
         }
     }
 
     @Transactional
     public List<Long> generatePdfByRangeOfDate(LocalDateTime initDateTime, LocalDateTime finalDateTime) {
+        log.info(String.format("BEGIN: generatePdfByRangeOfDate(%s, %s)", initDateTime, initDateTime));
         List<Long> invoiceCounters = invoiceRepository.findByDateTimeBetween(initDateTime, finalDateTime).stream().
                 map(Invoice::getInvoiceCounter).collect(Collectors.toList());
         invoicePdfGeneratorProducer.publish(invoiceCounters.stream().toArray(Long[]::new));
+        log.info(String.format("END: generatePdfByRangeOfDate(%s, %s)", initDateTime, initDateTime));
         return invoiceCounters;
     }
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public Optional<byte[]> getPdf(Long invoiceCounter) {
+        log.info(String.format("BEGIN: getPdf(%d)", invoiceCounter));
         byte[] binary = invoicePdfService.getPdfBinary(invoiceCounter);
 
+        Optional<byte[]> optionalBinary = Optional.empty();
         if (binary != null) {
-            return Optional.of(binary);
+            log.info(String.format("EMPTY: getPdf(%d)", invoiceCounter));
+            optionalBinary = Optional.of(binary);
         }
-        return Optional.empty();
+
+        log.info(String.format("END: getPdf(%d)", invoiceCounter));
+        return optionalBinary;
     }
 }
